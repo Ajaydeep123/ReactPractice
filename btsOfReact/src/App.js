79adf43540ad06e58208272aedbda83e2eeac22f
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-27 03:17:44 +0530

    ⚡️ useCallback

--- btsOfReact/src/App.js
+++ btsOfReact/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -9,20 +9,20 @@ function App() {
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
  
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />   
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
 
 export default App;
-
 //false is boolean so technically it will not trigger re evaluation but,
-// toggleParagraphHandler is a function passed as a prop in button, so it will trigger revaluation
+// toggleParagraphHandler is a function passed as a prop in button, so it will trigger revaluation, To avoid that we use useCallback hook.
+// useCallback is a React Hook that lets you cache a function definition between re-renders.
