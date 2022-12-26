73e98e82d8a5ff9313582aa1d240ba6beed06c0d
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-27 03:20:38 +0530

    usecallback dependency usecase

--- btsOfReact/src/App.js
+++ btsOfReact/src/App.js
@@ -6,17 +6,25 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
- 
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
@@ -26,3 +34,6 @@ export default App;
 //false is boolean so technically it will not trigger re evaluation but,
 // toggleParagraphHandler is a function passed as a prop in button, so it will trigger revaluation, To avoid that we use useCallback hook.
 // useCallback is a React Hook that lets you cache a function definition between re-renders.
+
+
+//  when the function that is stored in callback, changes by some outer parameter(allowToggle), then we need to use that parameter as a dependency in useCallback
