565996956a2615e408507a7fccdf2c8377570ef5
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-27 02:49:37 +0530

    📝 React.memo and its problem

---
+++ btsOfReact/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+ 
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={false} />   
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
+
+//false is boolean so technically it will not trigger re evaluation but,
+// toggleParagraphHandler is a function passed as a prop in button, so it will trigger revaluation
