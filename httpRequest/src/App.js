5ddc19d1ca0acc578e2f2560221b6a0f8294d313
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-29 01:17:38 +0530

    🔥 http requests

---
+++ httpRequest/src/App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  function fetchMoviesHandler(){
+    fetch('https://swapi.dev')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      const transformedData= data.results.map((movieData)=>{
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedData);
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
