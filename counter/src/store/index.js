f67df0de7b29e9cd4e949498d64dce423f4e8f1d
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-03 04:43:16 +0530

    ✨  getting data from store to our comp

--- counter/src/store/index.js
+++ counter/src/store/index.js
@@ -1,17 +1,19 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state={counter:0}, action) =>{
-if(action.type === 'increment'){
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
     return {
-        counter: counter+1,
+      counter: state.counter + 1,
     };
-}
-if(action.type === 'decrement'){
+  }
+
+  if (action.type === 'decrement') {
     return {
-        counter: counter-1,
+      counter: state.counter - 1,
     };
-}
-return state;
+  }
+
+  return state;
 };
 
 const store = createStore(counterReducer);
