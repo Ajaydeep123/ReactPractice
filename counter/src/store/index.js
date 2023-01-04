a3c6fd9baa2016412cce7d8562672159f65670ff
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-04 23:50:06 +0530

    ✨ redux toolkit using with createSlices

--- counter/src/store/index.js
+++ counter/src/store/index.js
@@ -1,43 +1,30 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
-    counter: 0, 
-    showCounter: true
- };   
+const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
   }
+});
 
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    };
-  }
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  //different state action
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+export const counterActions = counterSlice.actions; // redux automatically creates action objects
 
 export default store;
