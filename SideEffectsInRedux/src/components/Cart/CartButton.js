4ebb20866eaf13fc0e71ea2ea01eb4930046d343
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-05 05:49:04 +0530

    :lipstick: node-modules updates, nothing much

--- SideEffectsInRedux/src/components/Cart/CartButton.js
+++ SideEffectsInRedux/src/components/Cart/CartButton.js
@@ -1,8 +1,19 @@
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import {useDispatch} from 'react-redux';
+
+
 
 const CartButton = (props) => {
+  const dispatch = useDispatch(); 
+
+  const toggleCartHandler =() =>{
+    //dispatch toggle action, so we'll need to get uiAction and useDispatch hook to dispatch the action object
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
