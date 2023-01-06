84d290c1afeefccc3dc81aa4237aaaf0ed554f9e
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-06 10:28:02 +0530

    📝 cart implemented

--- SideEffectsInRedux/src/components/Cart/CartButton.js
+++ SideEffectsInRedux/src/components/Cart/CartButton.js
@@ -1,21 +1,20 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { uiActions } from '../../store/ui-slice';
-import {useDispatch} from 'react-redux';
-
-
+import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
-  const toggleCartHandler =() =>{
-    //dispatch toggle action, so we'll need to get uiAction and useDispatch hook to dispatch the action object
+  const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
   };
 
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
